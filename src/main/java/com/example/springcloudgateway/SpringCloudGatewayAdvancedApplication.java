564f8a5acd387fc30b429182f052f5d0f4b18877
Ramazan Güner <ramazan.guner@atmosware.turkcell.com.tr>
2025-06-27 15:24:06 +0300

    cache response filter

--- src/main/java/com/example/springcloudgateway/SpringCloudGatewayAdvancedApplication.java
+++ src/main/java/com/example/springcloudgateway/SpringCloudGatewayAdvancedApplication.java
@@ -3,6 +3,9 @@ package com.example.springcloudgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+https://www.baeldung.com/spring-cloud-gateway-webfilter-factories
+ */
 @SpringBootApplication
 public class SpringCloudGatewayAdvancedApplication {
 
